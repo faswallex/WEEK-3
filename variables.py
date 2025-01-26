e904cdb789cffbfa985cd04d6254fe985e3bd05d
faswallex <oifasanya@gmail.com>
2025-01-26 14:54:43 -0330

    first commit

---
+++ variables.py
@@ -0,0 +1,9 @@
+# Declare variables
+age = 40  
+height = 1.68  
+name = "Olawale Fasanya"  
+
+# Print variables with descriptive messages
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"My height is {height} meters.")
